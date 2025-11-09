cd058bc085dec3e6acd91473a595504b75c45f0b
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-11-08 19:32:41 -0600

    add backend

---
+++ backend/controllers/users.js
@@ -0,0 +1,102 @@
+bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+const {
+  sendBadRequest,
+  sendNotFound,
+  sendInternalError,
+  sendCreate,
+  sendConflict,
+  sendUnauthorized,
+} = require("../utils/errors");
+
+const { JWT_SECRET } = require("../utils/config");
+
+// CREATE - POST
+
+const createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!email) {
+      return sendBadRequest(res, "Email is required");
+    }
+
+    if (!password) {
+      return sendBadRequest(res, "Password is required");
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return sendConflict(res, "Email already exists");
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hash,
+    });
+
+    const userWithoutPassword = await User.findById(user._id)
+      .select("-password")
+      .lean();
+
+    return sendCreate(res, userWithoutPassword);
+  } catch (err) {
+    console.error(err);
+    if (err.code === 11000) {
+      return sendConflict(res, "Email already exists");
+    }
+    if (err.name === "ValidationError") {
+      return sendBadRequest(res, "Validation Failed");
+    }
+    return sendInternalError(res, "An error has occurred on the server");
+  }
+};
+
+//READ - GET
+
+const getCurrentUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const user = await User.findById(userId)
+      .select("-password")
+      .orFail()
+      .lean();
+
+    return res.json(user);
+  } catch (err) {
+    console.error(err);
+    if (err.name === "DocumentNotFoundError") {
+      return sendNotFound(res, "User not found");
+    }
+    return sendInternalError(res, "An error has occurred on the server");
+  }
+};
+
+// LOGIN - POST
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return sendBadRequest(res, "Email and password are required");
+    }
+
+    const user = await User.findUserByCredentials(email, password);
+
+    const token = JWT_SECRET.sign({ _id: user._id }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    return res.send({ token });
+  } catch (err) {
+    console.error(err);
+    return sendUnauthorized(res, "Invalid email or password");
+  }
+};
+
+module.exports = { createUser, getCurrentUser, login };
