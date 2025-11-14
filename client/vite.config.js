1b464d6926bbc28696290a2ce05957a4bfd60f7b
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-11-13 21:32:39 -0600

    remove comment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
-// client/vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Use a function that receives the 'mode' parameter from Vite
 export default defineConfig({
   plugins: [react()],
   server: {
