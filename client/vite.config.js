d83093268837cbfe06f65ff7bd7ce2f996cbb6a4
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-11-13 21:01:24 -0600

    Add build outDir to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,21 @@
 // client/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // Use a function that receives the 'mode' parameter from Vite
 export default defineConfig({
-    plugins: [react()],
-    server: {
-      open: true,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3001',
-          changeOrigin: true,
-          secure: false,
-        },
+  plugins: [react()],
+  server: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
       },
     },
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
